2ee3e6d401411ee9f2a893a92645a2abc1bad37d
Erwin Dupont <erwin.dupont@aca-it.be>
2022-11-10 09:24:49 +0100

    Introduced XmlPrinter to make it's functionality reusable

---
+++ src/test/java/be/edu/bggclient/internal/XmlPrinter.java
@@ -0,0 +1,23 @@
+package be.edu.bggclient.internal;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.dom4j.Node;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+
+public final class XmlPrinter {
+    private XmlPrinter() {
+        throw new UnsupportedOperationException("This static class cannot be instantiated.");
+    }
+
+    public static String prettyPrint(Node xmlNode) {
+        try (StringWriter stringWriter = new StringWriter()) {
+            new XMLWriter(stringWriter, OutputFormat.createPrettyPrint()).write(xmlNode);
+            return stringWriter.toString();
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
--- src/test/java/be/edu/bggclient/internal/geeklist/GeeklistXmlNodeTest.java
+++ src/test/java/be/edu/bggclient/internal/geeklist/GeeklistXmlNodeTest.java
@@ -2,10 +2,10 @@ package be.edu.bggclient.internal.geeklist;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 
 import be.edu.bggclient.geeklist.Geeklist;
 import be.edu.bggclient.geeklist.GeeklistItem;
+import be.edu.bggclient.internal.XmlPrinter;
 import be.edu.bggclient.internal.xml.XmlFormatter;
 import be.edu.bggclient.internal.xml.XmlInput;
 import be.edu.bggclient.internal.xml.XmlNode;
@@ -13,8 +13,6 @@ import be.edu.bggclient.internal.xml.XslStylesheet;
 import org.approvaltests.JsonApprovals;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.XMLWriter;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Node;
 
@@ -26,7 +24,26 @@ class GeeklistXmlNodeTest {
         JsonApprovals.verifyAsJson(new GeeklistXmlNode(readGeeklistXml()).build());
     }
 
-    private Element toXml(Geeklist geeklist) {
+    @Test
+    void pojoToXmlMatchesCleanedOriginalXml() {
+        Node xmlNode = readGeeklistXml();
+
+        String prettyPrint = XmlPrinter.prettyPrint(toXmlNode(new GeeklistXmlNode(xmlNode).build()));
+        String cleanedXml = new XslStylesheet("/be/edu/bggclient/internal/geeklist/geeklist-xml-cleanup.xsl").apply(xmlNode);
+        assertThat(prettyPrint).isEqualToIgnoringWhitespace(cleanedXml);
+    }
+
+    private Node readGeeklistXml() {
+        try (InputStream xml = GeeklistXmlNodeTest.class.getResourceAsStream("geeklist.xml")) {
+            assertThat(xml).isNotNull();
+            // TODO_EDU support multiple geeklists?
+            return XmlNode.nodes(new XmlInput().read(xml), "//geeklist").findFirst().orElseThrow();
+        } catch (IOException ioe) {
+            throw new IllegalStateException(ioe);
+        }
+    }
+
+    private Element toXmlNode(Geeklist geeklist) {
         Element geeklistNode = DocumentHelper.createElement("geeklist");
         geeklistNode.addAttribute("id", geeklist.getId());
         geeklistNode.addElement("postdate").addText(XmlFormatter.format(geeklist.getPostDate()));
@@ -36,11 +53,11 @@ class GeeklistXmlNodeTest {
         geeklistNode.addElement("username").addText(XmlFormatter.format(geeklist.getUsername()));
         geeklistNode.addElement("title").addText(XmlFormatter.format(geeklist.getTitle()));
         geeklistNode.addElement("description").addText(XmlFormatter.format(geeklist.getDescription()));
-        geeklist.getItems().forEach(geeklistItem -> geeklistNode.add(toXml(geeklistItem)));
+        geeklist.getItems().forEach(geeklistItem -> geeklistNode.add(toXmlNode(geeklistItem)));
         return geeklistNode;
     }
 
-    private Element toXml(GeeklistItem item) {
+    private Element toXmlNode(GeeklistItem item) {
         Element itemNode = DocumentHelper.createElement("item");
         itemNode.addAttribute("id", XmlFormatter.format(item.getId()));
         itemNode.addAttribute("objecttype", XmlFormatter.format(item.getObjectType()));
@@ -54,32 +71,5 @@ class GeeklistXmlNodeTest {
         itemNode.addAttribute("imageid", XmlFormatter.format(item.getImageId()));
         itemNode.addElement("body").addText(item.getComments());
         return itemNode;
-    }
-
-    private String prettyPrint(Node xmlNode) {
-        try (StringWriter stringWriter = new StringWriter()) {
-            new XMLWriter(stringWriter, OutputFormat.createPrettyPrint()).write(toXml(new GeeklistXmlNode(xmlNode).build()));
-            return stringWriter.toString();
-        } catch (IOException e) {
-            throw new IllegalArgumentException(e);
-        }
-    }
-
-    @Test
-    void pojoToXmlMatchesCleanedOriginalXml() {
-        Node xmlNode = readGeeklistXml();
-        String cleanedXml = new XslStylesheet("/be/edu/bggclient/internal/geeklist/geeklist-xml-cleanup.xsl").apply(xmlNode);
-
-        assertThat(prettyPrint(xmlNode)).isEqualToIgnoringWhitespace(cleanedXml);
-    }
-
-    private Node readGeeklistXml() {
-        try (InputStream xml = GeeklistXmlNodeTest.class.getResourceAsStream("geeklist.xml")) {
-            assertThat(xml).isNotNull();
-            // TODO_EDU support multiple geeklists?
-            return XmlNode.nodes(new XmlInput().read(xml), "//geeklist").findFirst().orElseThrow();
-        } catch (IOException ioe) {
-            throw new IllegalStateException(ioe);
-        }
     }
 }
